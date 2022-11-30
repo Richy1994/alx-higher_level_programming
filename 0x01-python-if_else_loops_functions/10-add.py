f45557be513fe47330ea830fcbf2ba4f1cbfee6f
Richard Ini Brown <indomitablerichy@gmail.com>
2022-11-29 21:30:25 -0800

    10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        print("{:d}".format(-(number % -10)), end='')
-        return(-(number % -10))
-    else:
-        print("{:d}".format(number % 10), end='')
-        return(number % 10)
+def add(a, b):
+    return(a + b)
