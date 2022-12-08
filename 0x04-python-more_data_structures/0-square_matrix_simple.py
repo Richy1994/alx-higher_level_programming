b8c6453a8cf558aed60ededd190840ddec719cd0
Richard Ini Brown <indomitablerichy@gmail.com>
2022-12-08 15:25:34 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,4 @@
 
 
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix)
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
